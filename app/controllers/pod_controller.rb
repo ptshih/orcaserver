52569b1532ac824e4134950bda5e1bd45ecafc57
Tom <tliou@tomair.local>
2011-06-12 20:17:31 -0700

    basic pod controller and pod model

--- app/controllers/pod_controller.rb
+++ app/controllers/pod_controller.rb
@@ -1,9 +1,48 @@
 class PodController < ApplicationController
   
+  # Get index of pods
+  # http://localhost:3000/v1/pod/index
   def index
+    
+    response = Pod.all
+    
+    # response = {:success => "true"}
+    respond_to do |format|
+      format.xml  { render :xml => response }
+      format.json  { render :json => response }
+    end
     
   end
   
+  # Get index of messages
+  def message_index
+    
+    Rails.logger.info request.query_parameters.inspect
+    
+  end
   
+  # Create new pod
+  def new
+    
+    response = {:success => "true"}
+    respond_to do |format|
+      format.xml  { render :xml => response }
+      format.json  { render :json => response }
+    end
+    
+  end
+  
+  # Create message
+  def message_new
+    
+    
+    
+    response = {:success => "true"}
+    respond_to do |format|
+      format.xml  { render :xml => response }
+      format.json  { render :json => response }
+    end
+    
+  end
   
 end
--- app/models/pod.rb
+++ app/models/pod.rb
@@ -1,5 +1,31 @@
-class Pod < ActiveRecord:Base
+class Pod < ActiveRecord::Base
   
+  def self.all
+    
+    response_array = []
+    
+    query = "select * from pods"
+    mysqlresults = ActiveRecord::Base.connection.execute(query)
+    
+    mysqlresults.each(:as => :hash) do |row|
+      response_hash = {
+        :id => row['id'],
+        :name => row['name'],
+        :last_message_id => row['last_message_id'],
+        :created_at => row['created_at'],
+        :updated_at => row['updated_at']
+      }
+      response_array << response_hash
+    end
+    
+    return mysqlresults
+  end
   
+  def self.create_via_resque(pod_id, message_uuid, message)
+  
+  end
+  
+  def self.create_message(pod_id, message_uuid, message)
+  end
   
 end
