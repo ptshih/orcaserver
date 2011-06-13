b763674f857fb6b4a3b3a238d974786e6c5e8eff
Peter Shih <ptshih@gmail.com>
2011-06-12 19:47:46 -0700

    init 3.0.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orcapods::Application.config.secret_token = '24457e228b053211d01fae4b5c910fbc125c48d052f52d6e6d1cb461cc37ecbccc59d856f5c1c8ad7b272ed96a16a5828340de30ba0d550ef696fd201287ec31'
