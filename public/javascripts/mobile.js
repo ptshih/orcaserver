7d7df3d70ad86121a8fcaee12f4a72df1b1c81f2
nathan bohannon <bohannon.nathan@gmail.com>
2011-06-19 18:11:28 -0700

    ignore temp.html working mobile submit

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,65 +1,68 @@
 $(document).ready(function(){
 
-// $("#submit2").click(function() {
-//  var formData = $("#newcomment").serialize();
-//  $.ajax({
-//	type: "POST",
-//	url: 
-//	cache: false,
-//	data: formData,
-//	success: onSuccess
-//  });
+});// end document.ready
 
-//  return false;
-	
-// });
+function sendmessage(e) {
+	alert('function called');
+  var formData = $("#newcomment").serialize();
 
-$('#submit').click(function(e) {
-	lineheight();
+  if (formData.length > 0) {
+    $.ajax({
+		type: "POST",
+		url: "http://localhost:3000/v1/pods/1/messages/create?",
+		cache: false,
+		data: formData,
+		success: onSuccess
+  	});
+  }
 	e.preventDefault();
-});
+}
 
-});// document.ready
+function podbtn() {
+	alert('pod button clicked');
+	resetTextFields();
+}
 
 $.fn.autoresize = function() {
-	var minheight = $('#commentpost').height();
-	var textwidth = $('#commentpost').css('width');
+	var minheight = $('#message').height();
+	var textwidth = $('#message').css('width');
 	
 	var textdiv = $('<div></div>').css({
 		position: 'absolute',
 		top: -9999,
 		left: -9999,
 		width:  textwidth,
-		'font-size': $('#commentpost').css('font-size'),
-		'font-family': $('#commentpost').css('font-family'),
-		'line-height': $('#commentpost').css('line-height'),
+		'font-size': $('#message').css('font-size'),
+		'font-family': $('#message').css('font-family'),
+		'line-height': $('#message').css('line-height'),
 		resize: 'none',
 		display: 'none'
 	}).appendTo(document.body);
 	
 	var update = function(){
-		var text = $('#commentpost').attr('value').replace(/</g, '&lt;')
+		var text = $('#message').attr('value').replace(/</g, '&lt;')
 												  .replace(/>/g, '&gt;')
 												  .replace(/&/g, '&amp;');
 		
 		textdiv.html(text);
-		$('#commentpost').css({height: Math.max(textdiv.height() + 30, minheight)});
+		$('#message').css({height: Math.max(textdiv.height() + 30, minheight)});
 	}
-	$('#commentpost').change(update).keyup(update).keydown(update);
+	$('#message').change(update).keyup(update).keydown(update);
 }
 
 
-function onSuccess(data, status){
+function onSuccess(data){
 	resetTextFields();
+	alert('Success!!!!!!!');
 	//notifications ex. checkmark icon on success
 	data = $.trim(data);
-	if(data == "SUCCESS") {
-		//add checkmark .css div hidden mark remove? 
-	} else {
-		//add exclamation .css div mark add?
-	}
+	// if(data == "SUCCESS") {
+	// 	//add checkmark .css div hidden mark remove? 
+	// } else {
+	// 	//add exclamation .css div mark add?
+	// }
 }
 
 function resetTextFields(){
-	$('#commentpost').val('');
+	$('#message').val('');
 }
